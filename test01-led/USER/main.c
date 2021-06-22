cba512d7e4b32b6110b14b4c221ed02df71c7788
Ryan <ryan@rently.com>
2021-06-22 18:50:30 +0800

    formart document

--- test01-led/USER/main.c
+++ test01-led/USER/main.c
@@ -16,19 +16,19 @@
 
 int main(void)
 {
-    HAL_Init();                    	 	//初始化HAL库    
-    Stm32_Clock_Init(RCC_PLL_MUL9);   	//设置时钟,72M
-	delay_init(72);               		//初始化延时函数
-	LED_Init();							//初始化LED	
-	
-	while(1)
+	HAL_Init();						//初始化HAL库
+	Stm32_Clock_Init(RCC_PLL_MUL9); //设置时钟,72M
+	delay_init(72);					//初始化延时函数
+	LED_Init();						//初始化LED
+
+	while (1)
 	{
-        HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_RESET); 	//LED0对应引脚PB5拉低，亮，等同于LED0(0)
-        HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,GPIO_PIN_SET);   	//LED1对应引脚PE5拉高，灭，等同于LED1(1)
-        delay_ms(500);											//延时500ms
-        HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);   	//LED0对应引脚PB5拉高，灭，等同于LED0(1)
-        HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,GPIO_PIN_RESET); 	//LED1对应引脚PE5拉低，亮，等同于LED1(0)
-        delay_ms(500);                                      	//延时500ms 
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); //LED0对应引脚PB5拉低，亮，等同于LED0(0)
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_SET);	  //LED1对应引脚PE5拉高，灭，等同于LED1(1)
+		delay_ms(500);										  //延时500ms
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);	  //LED0对应引脚PB5拉高，灭，等同于LED0(1)
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_RESET); //LED1对应引脚PE5拉低，亮，等同于LED1(0)
+		delay_ms(500);										  //延时500ms
 	}
 }
 
