c46d0b707905feaed33b435afced2b9de7cbf06e
Ryan <ryan@rently.com>
2021-06-25 01:32:11 +0800

    config gpio button input and led output add usb HID driver

---
+++ test02-mouse-cmake/cmake-build-default/CMakeFiles/test02-mouse-cmake.elf.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Core/Startup/startup_stm32f103zetx.s.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd_ex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/USB_DEVICE/App/usb_device.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/USB_DEVICE/App/usbd_desc.c.obj"
+  "CMakeFiles/test02-mouse-cmake.elf.dir/USB_DEVICE/Target/usbd_conf.c.obj"
+  "test02-mouse-cmake.elf"
+  "test02-mouse-cmake.elf.manifest"
+  "test02-mouse-cmake.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/test02-mouse-cmake.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
