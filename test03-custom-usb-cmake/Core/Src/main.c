8db0220c204ddad1349ba499478d009008541cd0
Ryan <ryan@rently.com>
2021-07-06 16:20:22 +0800

    integrate uart

--- test03-custom-usb-cmake/Core/Src/main.c
+++ test03-custom-usb-cmake/Core/Src/main.c
@@ -19,6 +19,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
+#include "usart.h"
 #include "usb_device.h"
 #include "gpio.h"
 
@@ -87,7 +89,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USB_DEVICE_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
