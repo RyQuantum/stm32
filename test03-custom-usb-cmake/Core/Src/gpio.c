e3fca9f339676d82563cfc654e8db47a58fc4f88
Ryan <ryan@rently.com>
2021-07-08 16:02:27 +0800

    enable more gpio button enable O2 optimization

--- test03-custom-usb-cmake/Core/Src/gpio.c
+++ test03-custom-usb-cmake/Core/Src/gpio.c
@@ -18,11 +18,10 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include <usbd_custom_hid_if.h>
 #include "gpio.h"
 
 /* USER CODE BEGIN 0 */
-
+#include <usbd_custom_hid_if.h>
 /* USER CODE END 0 */
 
 /*----------------------------------------------------------------------------*/
@@ -48,8 +47,10 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pins : PC0 PC1 PC2 PC3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  /*Configure GPIO pins : PC0 PC1 PC2 PC3
+                           PC4 PC5 PC6 PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
@@ -67,6 +68,12 @@ void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI3_IRQn);
 
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
 }
 
 /* USER CODE BEGIN 2 */
@@ -77,7 +84,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     {
         for(i = 0; i < 64; i++)
         {
-            hid_test[i] = 126 - i * 2;
+            hid_test[i] = 2;
         }
         USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
     }
@@ -93,7 +100,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     {
         for(i = 0; i < 64; i++)
         {
-            hid_test[i] = i * 2;
+            hid_test[i] = 1;
         }
         USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
     }
@@ -102,6 +109,38 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         for(i = 0; i < 64; i++)
         {
             hid_test[i] = i;
+        }
+        USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
+    }
+    if (GPIO_Pin == GPIO_PIN_4)
+    {
+        for(i = 0; i < 64; i++)
+        {
+            hid_test[i] = 3;
+        }
+        USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
+    }
+    if (GPIO_Pin == GPIO_PIN_5)
+    {
+        for(i = 0; i < 64; i++)
+        {
+            hid_test[i] = 4;
+        }
+        USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
+    }
+    if (GPIO_Pin == GPIO_PIN_6)
+    {
+        for(i = 0; i < 64; i++)
+        {
+            hid_test[i] = 6;
+        }
+        USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
+    }
+    if (GPIO_Pin == GPIO_PIN_7)
+    {
+        for(i = 0; i < 64; i++)
+        {
+            hid_test[i] = 5;
         }
         USBD_CUSTOM_HID_SendReport_FS(hid_test, 64);
     }
